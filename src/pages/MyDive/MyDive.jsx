d37b0fba9e32b6134be7e2ebb51edd99c0b83896
Ryan Mansoor <rmansoor@dare.global>
2024-05-15 15:46:55 +0100

    Price list and moocho and saaji

--- src/pages/MyDive/MyDive.jsx
+++ src/pages/MyDive/MyDive.jsx
@@ -6,6 +6,8 @@ import dolphin from "./dolphin.jpeg";
 import equipment from "./equipment.webp";
 import fishing from "./fishing.webp";
 import fishingMorning from "./fishingmorning.jpeg";
+import moocho from "./moocho.jpeg";
+import sajee from "./sajee.jpeg";
 
 import styles from "./MyDive.module.css";
 
@@ -71,6 +73,156 @@ export const MyDive = () => {
           experience!
         </GridTextBox>
         <img src={bbq} alt="BBQ" />
+        <img src={moocho} alt="Man diver" />
+        <GridTextBox title="Hi, I am Moocho">
+          "Yo-ho! I'm Moocho, a captain and diver for two decades, exploring the
+          Maldives' depths year-round. From coral to currents, I navigate
+          seasonal shifts and animal migrations, dedicated to preserving our
+          marine wonders for future generations. Join me as we embark on a
+          voyage of conservation and discovery!"
+        </GridTextBox>
+        <GridTextBox title="Hi, I am Sajee">
+          "Hello, fellow adventurers! I'm Sajee, a dive aficionado with a
+          penchant for exploration. With fins on my feet and a sense of wonder
+          in my heart, I've traversed the depths of both the northern and
+          southern realms of the Maldives, uncovering hidden treasures beneath
+          the waves. Join me as we embark on unforgettable underwater journeys
+          together!"
+        </GridTextBox>
+        <img src={sajee} alt="Man with long hair" />
+        <GridTextBox title="Fun Dive Price">
+          <ul className={styles.priceList}>
+            <li>
+              Single dive with equipment <b>$75</b>
+            </li>
+            <li>
+              Single dive without equipment <b>$65</b>
+            </li>
+            <li>
+              3 Boat dives with equipment <b>$150</b>
+            </li>
+            <li>
+              3 Boat dives without Equipment <b>$165</b>
+            </li>
+          </ul>
+        </GridTextBox>
+        <GridTextBox title="Equipment Rental">
+          <ul className={styles.priceList}>
+            <li>
+              BCD <b>$10</b>
+            </li>
+            <li>
+              REG <b>$10</b>
+            </li>
+            <li>
+              COMP <b>$10</b>
+            </li>
+            <li>
+              WETSUIT <b>$10</b>
+            </li>
+            <li>
+              MASK & FINS <b>$5</b>
+            </li>
+            <li>
+              15L TANK <b>$15</b>
+            </li>
+          </ul>
+        </GridTextBox>
+        <GridTextBox title="Dive Packages">
+          <ul className={styles.priceList}>
+            <li>
+              <div>
+                <p>5 days group dive per pax</p>
+                <small>5pax, 5 days, 13 dives, beach trip</small>
+              </div>
+              <b>$800</b>
+            </li>
+            <li>
+              <div>
+                <p>3 days group dive per pax</p>
+                <small>5pax, 5 days, 13 dives, beach trip</small>
+              </div>
+              <b>$550</b>
+            </li>
+          </ul>
+        </GridTextBox>
+        <GridTextBox title="Activities">
+          <ul className={styles.priceList}>
+            <li>
+              <div>
+                <p>Beach BBQ Setup</p>
+                <small>MIN pax 8</small>
+              </div>
+              <b>$75</b>
+            </li>
+            <li>
+              <div>
+                <p>Dolphin Cruise</p>
+                <small>MIN Pax 8</small>
+              </div>
+              <b>$65</b>
+            </li>
+            <li>
+              <div>
+                <p>Local Island Tour</p>
+                <small>MIN Pax 10</small>
+              </div>
+              <b>$50</b>
+            </li>
+            <li>
+              <div>
+                <p>Trolling</p>
+                <small>MIN Pax 3</small>
+              </div>
+              <b>$200</b>
+            </li>
+            <li>
+              <div>
+                <p>Night Fishing</p>
+                <small>MIN Pax 10</small>
+              </div>
+              <b>$50</b>
+            </li>
+          </ul>
+        </GridTextBox>
+        <GridTextBox title="Dive Courses">
+          <ul className={styles.priceList}>
+            <li>
+              PADI Open Water Course <b>$500</b>
+            </li>
+            <li>
+              PADI Advanced Open Water <b>$450</b>
+            </li>
+            <li>
+              PADI Discover Scuba Diving<b> $100</b>
+            </li>
+            <li>
+              PADI Scuba Diver Course <b>$350</b>
+            </li>
+            <li>
+              PADI Referral Course <b>$350</b>
+            </li>
+          </ul>
+        </GridTextBox>
+        <GridTextBox title="Group of services">
+          <ul className={styles.priceList}>
+            <li>
+              PADI EFR <b>$250</b>
+            </li>
+            <li>
+              PADI Adventure Course <b>$350</b>
+            </li>
+            <li>
+              PADI Rescue Diver Course<b> $450</b>
+            </li>
+            <li>
+              PADI Dive Master Course <b>$1500</b>
+            </li>
+            <li>
+              PADI Specialty <b>$250</b>
+            </li>
+          </ul>
+        </GridTextBox>
       </GridPage>
     </div>
   );
